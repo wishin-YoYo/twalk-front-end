d13c62ed8d26b2dff51a0c79a161bace59c8579f
MarsMan13 <choigeon0513@gmail.com>
2022-11-14 18:04:05 +0900

    cgcg update

--- lib/maps_components/rider_info.dart
+++ lib/maps_components/rider_info.dart
@@ -35,7 +35,7 @@ class RiderInfo extends StatelessWidget {
             onPressed: () {},
             child: SvgPicture.asset(
               "assets/icons/chat.svg",
-              color: Colors.black,
+              color: Colors.white,
             ),
           ),
         ),
